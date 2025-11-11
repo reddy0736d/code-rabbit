e8916df2f163b245aec3e320f6bbacd6abfc5c23
reddy0736d <rohitchandrareddy.02@gmail.com>
2025-11-11 09:54:23 +0530

    date 2024-10-05 14:30:00 +0000 author Rohith Chandra Reddy Bh <rohit@example.com>

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ def add_numbers(a, b):
 
 def main():
     result = add_numbers(100, 200)
-    result = add_numbers("10", 20)  # wrong type
     print("The result is:", result)
     print("This is a new update")
     print("Updated by Rohit for testing CodeRabbit")
