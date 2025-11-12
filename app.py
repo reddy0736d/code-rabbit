f2374a8c43e4fede086e8c57f63d514ea458ef97
reddy0736d <rohitchandrareddy.02@gmail.com>
2025-11-12 14:49:37 +0530

    done

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ def add_numbers(a, b):
 def main():
     result = add_numbers(100, 200)
     print("The result is:", result)
-    print("This is a new update")
-    print("Updated by Rohit for testing CodeRabbit")
 
 if __name__ == "__main__":
     main()
